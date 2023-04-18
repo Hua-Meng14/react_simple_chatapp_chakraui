a61b1e74f82720fe1719e680d7b0abac0d13cb11
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-04-18 17:04:16 +0700

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Flex w="100%">
+      <Avatar size="lg" name="Huameng Lim" src="https://lh3.googleusercontent.com/a/AGNmyxYtc_o9veUk5W8V67OX3nXbh_CfYjarD93Htl8FFQ=s288">
+        <AvatarBadge boxSize="1.25em" bg="green.500" />
+      </Avatar>
+    </Flex>
+  );
+};
+
+export default Header;
