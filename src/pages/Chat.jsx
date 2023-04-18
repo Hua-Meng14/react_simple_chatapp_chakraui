9bea14348db4871fcf0a7af24c9a1963fd69b35b
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-04-18 17:01:08 +0700

    Chat.jsx add chat component

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,17 +1,52 @@
 import { Flex } from "@chakra-ui/react";
-import React, {useState} from "react";
-import Divider from "../components/Divider"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import Messages from "../components/Message"
+import React, { useState } from "react";
+import Divider from "../components/Divider";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Messages from "../components/Message";
 
 const Chat = () => {
-    const [messages, setMessages] = useState([
-        {from: "computer", text: "Hi, My Name is GMChat."},
-        {from: "me", text: "Hey there"},
-        {from: "me", text: "Myself Huameng Lim"},
-        {from: "computer", text: "Nice to meet you. You cna send me message and I'll reply you with same message."},
-    ])
+  const [messages, setMessages] = useState([
+    { from: "computer", text: "Hi, My Name is GMChat." },
+    { from: "me", text: "Hey there" },
+    { from: "me", text: "Myself Huameng Lim" },
+    {
+      from: "computer",
+      text: "Nice to meet you. You cna send me message and I'll reply you with same message.",
+    },
+  ]);
 
-    const [inputMessage,SetInputMessage] = useState("";)
-}
+  const [inputMessage, SetInputMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (!inputMessage.trim().length) {
+      return;
+    }
+    const data = inputMessage;
+
+    setMessages((old) => [...old, { from: "me", text: data }]);
+    SetInputMessage("");
+
+    setTimeout(() => {
+      setMessages((old) => [...old, { from: "computer", text: data }]);
+    }, 1000);
+  };
+
+  return (
+    <Flex w="100%" h="100%" justify="center">
+      <Flex w="40%" h="90%" flexDir="column">
+        <Header />
+        <Divider />
+        <Messages messages={messages} />
+        <Divider />
+        <Footer
+          inputMessage={inputMessage}
+          setInputMessage={SetInputMessage}
+          handleSendMessage={handleSendMessage}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Chat;
